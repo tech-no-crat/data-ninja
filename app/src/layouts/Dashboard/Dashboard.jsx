23e9317b20b434b18e4e179ed95b6229c6282b5d
Themis Papameletiou <themicp@gmail.com>
2018-05-12 14:35:02 +0300

    Copyrights

--- app/src/layouts/Dashboard/Dashboard.jsx
+++ app/src/layouts/Dashboard/Dashboard.jsx
@@ -50,7 +50,7 @@ class App extends React.Component {
       <div className={classes.wrapper}>
         <Sidebar
           routes={dashboardRoutes}
-          logoText={"Creative Tim"}
+          logoText={"Data Ninja"}
           logo={logo}
           image={image}
           handleDrawerToggle={this.handleDrawerToggle}
