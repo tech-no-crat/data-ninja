ebcbefec81711a9f5e5762ff9e80f6c3d82e46d3
gglou <georgios.christoglou14@imperial.ac.uk>
2018-05-12 22:18:31 +0300

    Remove unnecessary console.logs

--- api/ml_training.js
+++ api/ml_training.js
@@ -4,8 +4,6 @@ var DecisionTreeTraining = function(data, target) {
 	// Defines the size of the test data in respect to the given data.
 	test_index = Math.ceil(data.length * 0.3);
 
-	console.log(test_index);
-
 	testSet = data.slice(0, test_index);
 	trainingSet = data.slice(test_index, data.length);
 
@@ -24,11 +22,7 @@ var DecisionTreeTraining = function(data, target) {
 
 	classifier.train(trainingSet, trainingPredictions);
 
-	console.log(testSet);
-
 	var result = classifier.predict(testSet);
-
-	console.log("Reached that point");
 
 	return [classifier, CalculateErrorRate(testPredictions, result)];
 };
@@ -37,17 +31,12 @@ var CalculateErrorRate = function(classLabels, predictions) {
 	var misclassifications = 0;
 	for (var i = 0; i < predictions.length; i++) {
 		// Misclassification.
-		console.log(predictions[i], classLabels[i]);
-
 		if (predictions[i] != classLabels[i]) {
 			misclassifications++;
 		} 
 	}
-
-	console.log(misclassifications);
-	console.log(predictions.length);
-
 	return misclassifications / predictions.length;
 }
+
 exports.CalculateErrorRate = CalculateErrorRate;
 exports.DecisionTreeTraining = DecisionTreeTraining;
