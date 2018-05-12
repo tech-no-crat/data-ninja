8fad9530685ada119fe3fecd4a59e9b87ab67fbb
Themis Papameletiou <themicp@gmail.com>
2018-05-12 22:10:25 +0300

    Show results from Model

--- app/src/views/Models/Models.jsx
+++ app/src/views/Models/Models.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "material-ui";
 import List, { ListItem, ListItemText } from 'material-ui/List';
+import Card, { CardContent } from 'material-ui/Card';
 import axios from 'axios';
 import Typography from 'material-ui/Typography';
 import Dropzone from 'react-dropzone'
@@ -17,6 +18,7 @@ import {
 
 const styles = {
   dropzoneWrapper: {
+    marginTop: '20px',
     '& > div': {
       width: '100% !important',
       height: '80px !important',
@@ -31,6 +33,25 @@ const styles = {
 };
 
 class Models extends React.Component {
+  state = {
+    results: {
+      columns: ['column1', 'column2', 'column3', 'column4'],
+      data: [
+        {
+          'column1': 'column1 data',
+          'column2': 352,
+          'column3': 32,
+          'column4': 'other data',
+        },
+        {
+          'column1': 'column1 data',
+          'column2': 352, 'column3': 32,
+          'column4': 'other data',
+        }
+      ]
+    }
+  };
+
   onDrop = async files => {
     const modelID = this.props.match.params.id;
     const formdata = new FormData();
@@ -51,12 +72,12 @@ class Models extends React.Component {
     const { classes } = this.props;
 
     return (
-			<div>
-				<RegularCard
-					headerColor="blue"
-					cardTitle="Model 1"
-					cardSubtitle="Model details"
-					content={
+      <div>
+        <RegularCard
+          headerColor="blue"
+          cardTitle="Model 1"
+          cardSubtitle="Model details"
+          content={
             <div>
               <List>
                 <ListItem>
@@ -107,14 +128,42 @@ class Models extends React.Component {
                 </ExpansionPanelDetails>
               </ExpansionPanel>
             </div>
-					}
-				/>
+          }
+        />
+        {this.state.results ? (
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography color="textSecondary" component="h3" variant='headline'>
+              Results
+            </Typography>
+
+            <Table className={classes.resultsTable}>
+              <TableHead>
+                <TableRow>
+                  {this.state.results.columns.map((column, index) => (
+                    <TableCell key={index}>{column}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.results.data.map((item, index) => (
+                  <TableRow key={index}>
+                    {this.state.results.columns.map((column, index) => (
+                      <TableCell key={index}>{item[column]}</TableCell>
+                    ))}
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        ) : null}
         <div className={classes.dropzoneWrapper}>
           <Dropzone onDrop={this.onDrop.bind(this)} >
             <p>Drop your CSV file here or click to select the file to upload.</p>
           </Dropzone>
         </div>
-			</div>
+      </div>
     );
   }
 }
