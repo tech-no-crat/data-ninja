fa1ec32e5116976c99941217a89618784decafba
Themis Papameletiou <themicp@gmail.com>
2018-05-12 14:31:33 +0300

    React Template trimmed down

---
+++ app/src/views/Dashboard/Dashboard.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+// react plugin for creating charts
+import { withStyles, Grid } from "material-ui";
+
+import {
+  ItemGrid
+} from "components";
+
+import dashboardStyle from "assets/jss/material-dashboard-react/dashboardStyle";
+
+class Dashboard extends React.Component {
+  state = {
+    value: 0
+  };
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+  render() {
+    return (
+      <div>
+        <Grid container>
+          <ItemGrid xs={12} sm={6} md={3}>
+          </ItemGrid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Dashboard.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(dashboardStyle)(Dashboard);
